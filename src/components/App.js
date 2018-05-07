fed3ae6a01294907407322aca6c838d1f84da027
Esteves <esteves@edusites.com.br>
2018-05-07 10:26:04 -0300

    Removido a lógica de trabalhar com os componentes de baixo nível do redux e importado o componente Provider da lib react-redux que facilita muito a conexao entre componentes do React e Redux, encapsulando o componente raiz APP em Provider que recebe a store para facilitar o acesso a seus subcomponentes

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,16 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
-import { addRecipe } from '../actions';
-
+/**
+ * Removido os métodos de baixo nível do Redux por ser
+ * complexo e existir uma biblioteca chamada react-redux
+ * que simplifica todo esse processo.
+*/
 class App extends Component {
-  /**
-   *  Após criar o input iniciamos seu state iniciando a propriedade
-   *  calendar com um valor inicial null
-  */
-  state = {
-    calendar : null,
-  }
-  /**
-   * E após este componente ser montado faço o destruring da
-   * store passado como prop e armazeno em uma constante 
-   * local chamada store que vai representar a store vinda
-   * de index.js e assim posso executar os método de uma store.
-   * Neste caso estamos executando subscribe() sempre que haver
-   * uma mudança na store do Redux, que por sua vez atualizara
-   * a propriedade calendar do estado desse componente com o 
-   * estado atual da store do Redux.
-  */
-  componentDidMount(){
-    const {store} = this.props;
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      },
-    }))
-
-    this.input.value = ''
-  }
-
   render() {
     return (
       <div>
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello World!
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,26 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
+import {Provider} from 'react-redux';
 
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-//console.log(store.getState());
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+/**
+ * Para evitar de descer todos os subcomponents para passar a store
+ * criada com o createStore do redux quando um subcomponentes desses
+ * precisar acessa-la ou precisar enviar uma action, será necessário
+ * encapsular o componente raiz do React com o componente Provider
+ * que faz parte da lib de componentes do react-redux, e ao invés de
+ * passar a store para o component APP será passao ao Provider, dessa
+ * forma facilitando o acesso a store e envio de actions aos 
+ * subcomponetes do React.
+ */
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
